7482f6c12f9124c7d999b2f47c4f33ed78d0f0c5
pekate17202 <e.pestereva@g.nsu.ru>
2022-01-09 00:25:32 +0700

    time init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+function DateTime(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p className="date">{props.date}</p>
+  )
 }
 
-export default App;
+const withPrettyFormat = (Component) => {
+  return function (props) {
+    let prettyTime;
+    let minutesPassed = (Date.now() - Date.parse(props.date))/(1000*60);
+    if (minutesPassed < 60) {
+      prettyTime = minutesPassed.toFixed() + ' минут назад';
+    }
+    else if (minutesPassed/60 < 24) {
+      prettyTime = (minutesPassed/60).toFixed() + ' часов назад';
+    }
+    else {
+      prettyTime = (minutesPassed/60/24).toFixed() + ' дней назад';
+    }
+    return <Component {...props} date={prettyTime}/>
+  }
+}
+
+const DateTimePretty = withPrettyFormat(DateTime);
+
+function Video(props) {
+  return (
+    <div className="video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen/>
+      <DateTimePretty date={props.date} />
+    </div>
+  )
+}
+
+function VideoList(props) {
+  return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2022-01-08 03:05:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2022-01-07 014:05:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2018-02-03 23:16:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2018-01-03 12:10:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2018-01-01 16:17:00'
+    },
+    {
+      url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+      date: '2017-12-02 05:24:00'
+    },
+  ]);
+
+  return (
+    <VideoList list={list} />
+  );
+}
